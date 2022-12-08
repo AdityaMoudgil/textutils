311adb80f9ef6133b7a5068130758b213792c8ee
Aditya Moudgil <moudgiladitya08@gmail.com>
2022-12-08 12:46:11 +0530

    textform

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       {/* <Navbar/> */}
       <Navbar title="TextUtils" />
+      <div className="container my-3">
+      <TextForm  heading="Enter the text to analyse:" />
+      </div>
       
           </>
   );
