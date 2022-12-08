188e89e9c9b18167ba39fc11376eca1500f35858
Aditya Moudgil <moudgiladitya08@gmail.com>
2022-12-08 11:51:28 +0530

    search

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
     <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">Text Utils</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -16,23 +16,16 @@ function App() {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Pricing</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="*">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
+          <a className="nav-link" href="/">About</a>
         </li>
       </ul>
     </div>
+    <div className="container-fluid">
+    <form className="d-flex" role="search">
+      <input className="form-control me-5" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
   </div>
 </nav>
     </>
