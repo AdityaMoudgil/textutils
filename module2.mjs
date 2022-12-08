5925ef7a8ae91f22a644a419e3452b8dfee027c6
Aditya Moudgil <moudgiladitya08@gmail.com>
2022-12-08 12:18:44 +0530

    props & modules added

---
+++ module2.mjs
@@ -0,0 +1,5 @@
+const a = "Aditya";
+const b = "Adi";
+const c = "Amit";
+
+export default c;
--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Text Utils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-    </div>
-    <div className="container-fluid">
-    <form className="d-flex" role="search">
-      <input className="form-control me-5" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-  </div>
-</nav>
+      <Navbar title="TextUtils" aboutText="About TextUtils"/>
     </>
   );
 }
