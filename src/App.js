c13c860bd7a1e1ffbb4b11d4a11ab89b56c82f45
Aditya Moudgil <moudgiladitya08@gmail.com>
2022-12-11 17:42:19 +0530

    NavBar Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 //import logo from './logo.svg';
 import './App.css';
-import About from './Components/About';
+//import About from './Components/About';
 import Navbar from './Components/Navbar';
-//import TextForm from './Components/TextForm';
-
+import TextForm from './Components/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-      {/* <TextForm  heading="Enter the text to analyse:" /> */}
-      <About/>
+      <TextForm  heading="Enter the text to analyse:" mode={mode} />
+      {/* <About/> */}
       </div>
       
           </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -20,11 +20,17 @@ export default function TextForm(props) {
     setText(newText);
 }
 
-//   const handleAlternateClick = ()=>{
-//   //console.log("Text was cleared");
-//     let newText =;
-//     setText(newText);
-// }
+  const handleCopy = () => {
+    //console.log("I am copy");
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
 
     const handleOnChange = (event)=>{
         console.log("On Change");
@@ -49,7 +55,9 @@ export default function TextForm(props) {
      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
      <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
      <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-
+     <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+     <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+    
     </div>
 
     <div className="container my-3">
